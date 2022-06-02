791970d4eae8b3889b33a1435cc2878b37de993c
Aliza02 <alizaanxari08@gmail.com>
2022-06-02 14:46:44 +0500

    bakistry complete

--- menu.js
+++ menu.js
@@ -3,13 +3,13 @@ function rotate()
 {
     if(rotation===0)
    {
-        document.querySelector('#addition').style.transform="rotate(45deg)";
+        document.querySelector('.addition').style.transform="rotate(135deg)";
         rotation+=45;
         console.log(rotation);
    }
     else if(rotation===45){
         rotation=0;
-        document.querySelector('#addition').style.transform="rotate(90deg)";
+        document.querySelector('.addition').style.transform="rotate(0deg)";
         console.log(rotation);
     }
   
